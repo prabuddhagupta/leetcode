bd4116b6a61621644a100956a24d7668d0d715f4
Prabuddha Gupta <prabuddha@gmail.com>
2014-04-24 20:31:13 -0400

    removed string implementation

--- src/com/prabuddha/ReverseInteger.java
+++ src/com/prabuddha/ReverseInteger.java
@@ -6,16 +6,17 @@ package com.prabuddha;
 public class ReverseInteger {
 	public static int reverse(int number) {
 		int result=0;
-		String numStr = ""+number;
-		for(int i=0; i<numStr.length();i++){
-			int digit = Character.getNumericValue(numStr.charAt(i));
-			result = (int) (result+ (digit *  Math.pow(10.0, i)));
+		
+		while(number >0){
+			int lastDigit = number %10;
+			result = result*10 + lastDigit;
+			number = number/10; //number after stripping lastDigit off
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(ReverseInteger.reverse(123456789));
+		System.out.println(ReverseInteger.reverse(1234560));
 	}
 
 }
