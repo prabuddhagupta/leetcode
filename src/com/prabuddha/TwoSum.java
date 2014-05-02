3cc5c9f43cf0fae48f5e714fcae308df4ec48277
Prabuddha Gupta <prabuddha@gmail.com>
2014-05-02 15:17:29 -0400

    added some comment

--- src/com/prabuddha/TwoSum.java
+++ src/com/prabuddha/TwoSum.java
@@ -12,6 +12,8 @@ public class TwoSum {
 		Map<Integer, Integer> complementMap = new HashMap<Integer, Integer>();
 		int []result = new int[2];
 		for(int i=0; i<numbers.length; i++){
+			//save the difference (of target and current number) in the map. 
+			//Now we just need to look for that difference in the rest of the array.
 			if(complementMap.containsKey(numbers[i])){
 				result[0] = complementMap.get(numbers[i]) + 1;
 				result[1] = i+1;
